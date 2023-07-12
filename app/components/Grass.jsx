78f55592a9dfe71ffb09f3d0c3ae08ae2bdcf7a3
Esin <nbusra.esin@gmail.com>
2023-07-12 23:20:32 +0300

    change on spline component

--- app/components/Grass.jsx
+++ app/components/Grass.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 const Grass = () => {
   return (
-    <div>
+    <div
+    style={{
+        width: '700px',
+        height: '500px',
+        position: 'relative',
+        overflow: 'hidden',
+      }}
+    >
 <script type="module" src="https://unpkg.com/@splinetool/viewer@0.9.383/build/spline-viewer.js"></script>
 <spline-viewer loading-anim url="https://prod.spline.design/Qrk8lqN5sqyc4oqK/scene.splinecode"></spline-viewer>
     </div>
--- app/page.js
+++ app/page.js
@@ -3,10 +3,10 @@ import Grass from './components/Grass'
 
 const page = () => {
   return (
-    <div className='font-raleway h-screen flex flex-col justify-center items-center space-y-5 '>
+    <div className='font-raleway max-h-screen flex flex-col justify-center items-center space-y-5  '>
         <Grass />
-        <h1 className="text-xl text-stone-700"> <span className='italic'> toplumun </span> sanatı</h1>
-        <p className='text-center font-mono text-sm text-stone-500'> Resim sanatı toplumun kendini ifade etme yollarından biridir, <br></br> belirli bir zümrenin bir belirli zümreye yaptığı bir iş değil. </p>
+        <h1 className="text-4xl text-stone-700"> <span className='italic text-stone-600'> toplumun </span> sanatı</h1>
+        <p className='text-center font-mono text-sm text-stone-500'> Resim sanatı toplumun kendini ifade etme yollarından biridir, <br></br> belirli bir zümrenin belirli bir zümreye yaptığı bir iş değil. </p>
         <button className='rounded-2xl bg-stone-100 px-5 py-1 text-sm hover:bg-black hover:text-stone-100  '> başla </button>
     </div>
   )
