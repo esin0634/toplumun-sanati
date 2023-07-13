1af6c5b4ad97832c9a36f568078a9fedadd21aa9
Esin <nbusra.esin@gmail.com>
2023-07-13 12:42:06 +0300

    working on vercel deployement

--- app/components/Grass.jsx
+++ app/components/Grass.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Script from 'next/script';
+
 
 const Grass = () => {
   return (
@@ -10,7 +12,7 @@ const Grass = () => {
         overflow: 'hidden',
       }}
     >
-<script type="module" src="https://unpkg.com/@splinetool/viewer@0.9.383/build/spline-viewer.js"></script>
+<Script type="module" src="https://unpkg.com/@splinetool/viewer@0.9.383/build/spline-viewer.js" />
 <spline-viewer loading-anim url="https://prod.spline.design/Qrk8lqN5sqyc4oqK/scene.splinecode"></spline-viewer>
     </div>
   );
--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Grass from './components/Grass'
 
 const page = () => {
   return (
-    <div className='font-raleway max-h-screen flex flex-col justify-center items-center space-y-5  '>
+    <div className='font-raleway max-h-screen flex flex-col justify-center items-center space-y-5 p-5 m-5 '>
         <Grass />
         <h1 className="text-4xl text-stone-700"> <span className='italic text-stone-600'> toplumun </span> sanatı</h1>
         <p className='text-center font-mono text-sm text-stone-500'> Resim sanatı toplumun kendini ifade etme yollarından biridir, <br></br> belirli bir zümrenin belirli bir zümreye yaptığı bir iş değil. </p>
